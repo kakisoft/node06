db55a5a1c01397a4734eda36189a7b65ea327824
kakisoft <kakisttab@gmail.com>
2017-05-12 02:47:42 +0900

    1st commit

---
+++ node06.js
@@ -0,0 +1,45 @@
+const PORT = 1337
+
+var http = require('http');
+var fs = require('fs');
+var url = require('url');
+var jade = require('jade');
+
+var index = fs.readFileSync('./index.jade','utf8');
+var style = fs.readFileSync('./style.css','utf8');
+
+var server = http.createServer();
+server.on('request',doRequest);
+server.listen(PORT);
+
+function doRequest(req, res){
+    var path = url.parse(req.url);
+
+    switch(path.pathname){
+        case '/':
+            var fn = jade.compile(index);
+            var tmp = fn();
+
+            res.setHeader('Context-Type','text/html');
+            res.write(tmp);
+            res.end();
+            break;
+
+        case '/style.css': //For Style Sheet
+            res.setHeader('Content-Type','text/css');
+            res.write(style);
+            res.end();
+            break;
+        
+        case '/favicon.ico':
+            break;
+
+        default:
+            res.setHeader('Content-Type','text/plain');
+            res.write('NO PAGE!');
+            res.end();
+            break;
+    }
+}
+console.log('Server running at http://127.0.0.1'+PORT)
+
